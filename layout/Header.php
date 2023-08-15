28fcf6b04036be1238e10a7727602b89adc5d497
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-08-15 19:31:54 +0200

    add form registration

---
+++ layout/Header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+     
+    </head>
+    <style>
+        <?php include "../assets/css/style.css"; ?>
+    </style>
+    <body>
+    <header>
+    <nav class="navbar">
+        <a href=" /Social-Media-Website/ " class="logo text-decoration-none text-white fs-2">Valley</a>
+        <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="./page/Home.php" class="nav-link text-decoration-none text-white">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href=" " class="nav-link text-decoration-none text-white">add Friend</a>
+        </li>
+        <li class="nav-item">
+            <a href=" " class="nav-link text-decoration-none text-white">Message</a>
+        </li>
+        <li class="nav-item">
+            <a href="/Social-Media-Website/" class="nav-link text-decoration-none text-white">Logout</a>
+        </li>
+        </ul>
+        <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        </div>
+    </nav>
+    </header>
+    </body> 
+
+</html>
